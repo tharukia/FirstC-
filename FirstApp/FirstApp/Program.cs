0968276571aa46b73942b5054020c23f78f10e80
Tharuki Ariyapala <tariyapala3@gmail.com>
2019-09-10 13:49:44 +0530

    Testing Commit

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+            Console.Write("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
